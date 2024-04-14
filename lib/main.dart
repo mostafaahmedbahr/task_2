ef6dfd7092a73ef26637fd215f41925df157b128
khaledmaher2001 <khaled.maher709@gmail.com>
2024-04-14 15:25:33 +0200

    khaled 14/4

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
  import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:responsive_framework/responsive_wrapper.dart';
 import 'package:responsive_framework/utils/scroll_behavior.dart';
 import 'package:task_2/core/sh.dart';
